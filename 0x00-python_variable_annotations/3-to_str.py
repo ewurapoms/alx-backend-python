b2f9ed30de955b4e14fe2811ffa340c8a3ef1ee1
ewurapoms <mzpomaa35@gmail.com>
2024-02-15 04:51:34 -0800

    returns the string representation of the float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module returns the string representation of a float."""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a float.
+    """
+    return str(n)
