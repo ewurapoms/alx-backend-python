c936da55742f9a42438f24669c2251deacab507f
ewurapoms <mzpomaa35@gmail.com>
2024-02-15 05:14:44 -0800

    python_variable_annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module returns the sum of floats in a list"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns the sum of floats in a list"""
+    return sum(input_list)
---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Returns a tuple with the string k and the square of v """
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ """
+    return k, float(v ** 2)
