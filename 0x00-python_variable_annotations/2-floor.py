a850b5c2561a1cd145e5742ba5e64c6c152437f2
ewurapoms <mzpomaa35@gmail.com>
2024-02-15 04:50:21 -0800

    returns the floor of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module returns the floor value of a float."""
+
+
+def floor(n: float) -> int:
+    """Returns the floor value of a float.
+
+    Args:
+        n (float): The input float number.
+
+    Returns:
+        int: The floor value of the input number.
+    """
+    return int(n // 1)
