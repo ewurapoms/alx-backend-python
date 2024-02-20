3b6ce605acc5711936358ca9dad8c16e5b9ffecf
ewurapoms <mzpomaa35@gmail.com>
2024-02-20 04:30:11 -0800

    Updating await line in function

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -12,13 +12,6 @@ async_comprehension = using('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """displays function """
     start = time.time()
-
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
     end = time.time()
     return end - start
