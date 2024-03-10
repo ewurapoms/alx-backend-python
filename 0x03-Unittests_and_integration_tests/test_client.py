8fddcd16da6b0552c6ce213bc62e3efff69f91fa
ewurapoms <mzpomaa35@gmail.com>
2024-03-10 06:23:22 -0700

    debugging test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""A module for testing the client module.
-"""
+"""Module for tasks 4-9"""
+
 import unittest
 from typing import Dict
 from unittest.mock import (
@@ -9,32 +9,27 @@ from unittest.mock import (
     PropertyMock,
     patch,
 )
-from parameterized import parameterized, parameterized_class
 from requests import HTTPError
-
-from client import (
-    GithubOrgClient
-)
 from fixtures import TEST_PAYLOAD
+from client import GithubOrgClient
+from parameterized import parameterized, parameterized_class
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """Tests the `GithubOrgClient` class."""
-    @parameterized.expand([
-        ("google", {'login': "google"}),
-        ("abc", {'login': "abc"}),
-    ])
-    @patch(
-        "client.get_json",
+    """testing git class"""
+    @parameterized.expand(
+        [
+            ("google"),
+            ("abc"),
+        ]
     )
-    def test_org(self, org: str, resp: Dict, mocked_fxn: MagicMock) -> None:
-        """Tests the `org` method."""
-        mocked_fxn.return_value = MagicMock(return_value=resp)
-        gh_org_client = GithubOrgClient(org)
-        self.assertEqual(gh_org_client.org(), resp)
-        mocked_fxn.assert_called_once_with(
-            "https://api.github.com/orgs/{}".format(org)
-        )
+    @patch("client.get_json", return_value={"payload": True})
+    def test_org(self, org_name: str, mock_get: Mock) -> None:
+        """ tests the org class"""
+        github_org_client = GithubOrgClient(org_name)
+        self.assertEqual(github_org_client.org, {"payload": True})
+        url = f"https://api.github.com/orgs/{org_name}"
+        mock_get.assert_called_once_with(url)
 
     def test_public_repos_url(self) -> None:
         """Tests the `_public_repos_url` property."""
